eeff7eda4dc006c03b1646e5e5eb839aab712499
LeafyDev <will09600@gmail.com>
2016-02-19 15:29:03 +0100

    Init

---
+++ Leafy-IRCBot.Quotes/Classes/Config.cs
@@ -0,0 +1,35 @@
+﻿// -----------------------------------------------------------
+// This program is private software, based on C# source code.
+// To sell or change credits of this software is forbidden,
+// except if someone approves it from the LeafyCoding INC. team.
+// -----------------------------------------------------------
+// Copyrights (c) 2016 Leafy-IRCBot.Quotes INC. All rights reserved.
+// -----------------------------------------------------------
+
+namespace IRCBot.Quotes.Classes
+{
+    internal class Config
+    {
+        //private static IniConfigSource ConfigFile;
+
+        public static string IRC_BotName = string.Empty;
+        public static string IRC_ChannelName = string.Empty;
+        public static string IRC_Server = string.Empty;
+        public static string IRC_Password = string.Empty;
+        public static string IRC_NSPassword = string.Empty;
+        public static bool IRC_SSL;
+
+        public static string DB_User = string.Empty;
+        public static string DB_Address = string.Empty;
+        public static string DB_Password = string.Empty;
+        public static string DB_Database = string.Empty;
+        public static string DB_Port = string.Empty;
+
+        public static string Twitter_Consumer = string.Empty;
+        public static string Twitter_ConsumerSecret = string.Empty;
+        public static string Twitter_Token = string.Empty;
+        public static string Twitter_TokenSecret = string.Empty;
+
+        public static bool Init() => true; // TODO
+    }
+}
---
+++ Leafy-IRCBot.Quotes/Program.cs
@@ -0,0 +1,26 @@
+﻿// -----------------------------------------------------------
+// This program is private software, based on C# source code.
+// To sell or change credits of this software is forbidden,
+// except if someone approves it from the LeafyCoding INC. team.
+// -----------------------------------------------------------
+// Copyrights (c) 2016 Leafy-IRCBot.Quotes INC. All rights reserved.
+// -----------------------------------------------------------
+
+using System;
+using IRCBot.Quotes.Classes;
+
+namespace IRCBot.Quotes
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            if (!Config.Init())
+            {
+                Environment.Exit(1);
+            }
+
+
+        }
+    }
+}
